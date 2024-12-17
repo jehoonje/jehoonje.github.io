2777976ceadb8bce30b8b6118ffad00d282b1c86
Jehoon <limjhoon8@gmail.com>
2024-12-17 13:59:52 +0900

    Deploy React app using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
